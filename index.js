b031914a5b5d329e3adc93b8f4454e4caae53eb9
“Mason <masonmadaras@gmail.com>
2022-08-16 16:02:46 -0400

    typescript examples

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
 // define the variables value/identifier
-let firstname: string = "Mason"
-let age: number = 25
-let active: Boolean = true
-
+var firstname = "Mason";
+var age = 25;
+var active = true;
 // let names: string[] = [1,2,3,4] // gives us a error
-let names: string[] = ["bobby,lilly,jennifer"]
-
+var names = ["bobby,lilly,jennifer"];
 // enum
-enum studentTypes {
-    pending,  // 0
-    active,   // 1
-    almostdone,  // 2
-    graduated,   // 3
-    tas,         // 4
-    teachers     // 5
-
-}
-
-
+var studentTypes;
+(function (studentTypes) {
+    studentTypes[studentTypes["pending"] = 0] = "pending";
+    studentTypes[studentTypes["active"] = 1] = "active";
+    studentTypes[studentTypes["almostdone"] = 2] = "almostdone";
+    studentTypes[studentTypes["graduated"] = 3] = "graduated";
+    studentTypes[studentTypes["tas"] = 4] = "tas";
+    studentTypes[studentTypes["teachers"] = 5] = "teachers"; // 5
+})(studentTypes || (studentTypes = {}));
 // let mystatus = studentTypes.graduated
-
 // switch (Number(mystatus)) {
 //     case Number(studentTypes.active):
 //         break;
@@ -29,63 +24,40 @@ enum studentTypes {
 //     default:
 //         console.log("😒")
 // }
-
 // console.log(studentTypes)
-
-
 // console.log("my status is", mystatus);
 // if (mystatus == studentTypes.graduated) {
 //     console.log('The Student is graduated')
 // }
-
 // console.log(names)
 // console.log(`my first name is ${firstname}`)
-
 // // string or numbers or both can be return
 // let code: string | number = 11
 // code = 'AA11'
 // // code = false // error not a string or number
-
 // console.log(`my Entry code is: ${code}`)
-
-function sum(arr: number[]) {
-     return arr.reduce((total: number, num: number) => total + num)
+function sum(arr) {
+    return arr.reduce(function (total, num) { return total + num; });
     // arr.reduce( (previousValue: number, currentValue: number) => {return previousValue + currentValue})
 }
-
-let numbers: number[] = [1, 2, 3, 4, 5, 6]
-
+var numbers = [1, 2, 3, 4, 5, 6];
 // console.log(sum(numbers))
-let complexItem: any = { name: "mason"}
-
-complexItem = { name: "Mason", hasDog: false}
+var complexItem = { name: "mason" };
+complexItem = { name: "Mason", hasDog: false };
 // complexItem.name = "Todd"
 // complexItem = "It's a String"
 // complexItem = 22
-
 // console.log("complexItem",complexItem)
-
-enum bloodType {
-    aPostive,
-    bPostive,
-    oNegative,
-    oPositive,
-} 
-
-interface human {
-    name: string,
-    age: number,
-    bloodType?: bloodType
-    height: number,
-} 
-
-
-const me : human = {
+var bloodType;
+(function (bloodType) {
+    bloodType[bloodType["aPostive"] = 0] = "aPostive";
+    bloodType[bloodType["bPostive"] = 1] = "bPostive";
+    bloodType[bloodType["oNegative"] = 2] = "oNegative";
+    bloodType[bloodType["oPositive"] = 3] = "oPositive";
+})(bloodType || (bloodType = {}));
+var me = {
     name: "Bobby",
     "age": 30,
     // bloodType: bloodType.oNegative,
     height: 6 * 12
-
-}
-
-console.log({me})
+};
